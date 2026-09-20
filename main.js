1e685d21b9ce24f4258df9a6e5ee81f9a433b9c0
ionide <mar109us@gmail.com>
2026-09-20 19:40:56 +0200

    trying to remove commas

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 const main = {
-   view: document.getElementById("app")
-}
+   view: document.getElementById("app"),
+};
 
+const screen = {
+   x0: [0, 0, 0, 0, 0, 0, 0, 0],
+   x1: [0, 0, 0, 0, 0, 0, 0, 0],
+   x2: [0, 0, 0, 0, 0, 0, 0, 0],
+   x3: [0, 0, 0, 0, 0, 0, 0, 0],
+   x4: [0, 0, 0, 0, 0, 0, 0, 0],
+   x5: [0, 0, 0, 0, 0, 0, 0, 0],
+   x6: [0, 0, 0, 0, 0, 0, 0, 0],
+   x7: [0, 0, 0, 0, 0, 0, 0, 0],
+};
+
+function updateView() {
+   main.view.innerHTML = drawScreen();
+}
+updateView();
+
+function drawScreen() {
+   let currenctScreen = ""
+   for (let row in screen) {
+         console.log(row);
+         currenctScreen += `<div>${screen[row]}</div>`;
+   }
+   console.log(currenctScreen);
+   currenctScreen.replace("0", "1")
+   return currenctScreen;
+}
