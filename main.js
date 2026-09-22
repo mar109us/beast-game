dc93a3dbf131e58e06be41f756638c7605ff09ce
ionide <mar109us@gmail.com>
2026-09-22 22:17:55 +0200

    creating map with a more tile-like system

--- main.js
+++ main.js
@@ -3,7 +3,27 @@ const main = {
 };
 
 const screen = {};
-const gameMap = {
+const map = {
+   1: {
+      0: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+      1: [0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      2: [0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      3: [0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      4: [0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      5: [0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      6: [0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      7: [0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      8: [0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      9: [0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      10: [0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      11: [0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      12: [0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      13: [0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      14: [0, 3, 3, 3, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      15: [0, 3, 3, 3, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      16: [0, 3, 3, 3, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+      17: [0, 0, 3, 3, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+   },
    width: 20,
    height: 18,
 };
@@ -25,6 +45,15 @@ const input = {
    },
 };
 
+const tile = {
+   0: "<div class='tile wall'>xxx<br>xxx</div>",
+   1: "<div class='tile path'></div>",
+   2: "<div class='tile grass'>, ,,  ,<br>  , , </div>",
+   3: "<div class='tile grass'>,,,,,,<br>,,,,,,</div>",
+   4: "<div class='tile water'>~~~<br>~~~</div>",
+   999: "<div class='tile character'>:D</div>",
+};
+
 createRow();
 
 function updateView() {
@@ -39,8 +68,8 @@ function drawScreen() {
    for (let row = viewport.yAnchor; row < viewport.height + viewport.yAnchor; row++) {
       currentScreen += `<div class="row">`;
       for (let column = viewport.xAnchor; column < viewport.width + viewport.xAnchor; column++) {
-         if (row === 4 + viewport.yAnchor && column === 4 + viewport.xAnchor) currentScreen += `<span class="tile character">${drawCharacter()}</span>`;
-         else currentScreen += `<span class="tile">${drawTile(column, row)}</span>`;
+         if (row === 4 + viewport.yAnchor && column === 4 + viewport.xAnchor) currentScreen += tile[999];
+         else currentScreen += `<div class="tile">${drawTile(column, row)}</div>`;
          // currentScreen += `<span class="tile">${column}, ${row}</span>`;
       }
       currentScreen += `</div>`;
@@ -64,24 +93,25 @@ function createButtons() {
 }
 
 function createRow() {
-   for (let i = 0; i < gameMap.height; i++) {
+   for (let i = 0; i < map.height; i++) {
       screen[i] = createColumn();
    }
 }
 
 function createColumn() {
    let currentArray = [];
-   for (let i = 0; i < gameMap.width; i++) {
+   for (let i = 0; i < map.width; i++) {
       currentArray.push(0);
    }
    return currentArray;
 }
 
 function drawTile(column, row) {
-   if (column < 0 || column > gameMap.width - 1 || row < 0 || row > gameMap.height - 1) return `<div class="void"></div>`;
-   if (column === 0 || column === gameMap.width - 1) return "xxxx<br>xxxx";
-   if (row === 0 || row === gameMap.height - 1) return "xxxx<br>xxxx";
-   else return "0";
+   if (column < 0 || column > map.width - 1 || row < 0 || row > map.height - 1) return `<div class="void"></div>`;
+   else return tile[map[1][row][column]]
+   // if (column === 0 || column === map.width - 1) return "xxxx<br>xxxx";
+   // if (row === 0 || row === map.height - 1) return "xxxx<br>xxxx";
+   // else return "0";
 }
 
 function drawCharacter() {
@@ -91,12 +121,12 @@ function drawCharacter() {
 function move(direction) {
    let button = input.character.walkDirection;
    if (button.right.includes(direction)) {
-      if (viewport.xAnchor !== gameMap.width - viewport.width + 3) {
+      if (viewport.xAnchor !== map.width - viewport.width + 3) {
          viewport.xAnchor = viewport.xAnchor += 1;
       } else console.log("Boundary hit: right");
    }
    if (button.down.includes(direction)) {
-      if (viewport.yAnchor !== gameMap.height - viewport.height + 3) {
+      if (viewport.yAnchor !== map.height - viewport.height + 3) {
          viewport.yAnchor = viewport.yAnchor += 1;
       } else console.log("Boundary hit: bottom");
    }
